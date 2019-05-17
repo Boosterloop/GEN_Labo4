34909923f9d9b65dd9000209ecff5e1eb72684ef
Boosterloop <alison.savary@me.com>
2019-05-17 15:55:49 +0200

    Déplacement de getContents() : OrdersWriter --> Orders

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -1,24 +1,10 @@
 package ch.heigvd.gen2019;
 
 public class OrdersWriter {
-    private Orders orders;
+    Orders orders;
 
     public OrdersWriter(Orders orders) {
         this.orders = orders;
-    }
-
-    public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            orders.getOrder(i).orderToJsonString(sb);
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
     }
 
 }
